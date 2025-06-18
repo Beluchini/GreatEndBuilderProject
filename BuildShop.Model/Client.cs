e29257425bb76353d7a61f0680c7f82c7a9e65d1
Beluchini <pavelg25052007@gmail.com>
2025-06-18 20:59:20 +0400

    Отредактировал client, обновил БД, нет дизайна корзины

--- BuildShop.Model/Client.cs
+++ BuildShop.Model/Client.cs
@@ -7,4 +7,10 @@ public class Client
     [Key] public int Id { get; set; }
     public string NickName { get; set; }
     public string Email { get; set; }
+    public string Name { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Card { get; set; }
+    public string Code { get; set; }
+    public string DateCard { get; set; }
 }
